b62693d68475aa99080575bda474a4f6aa1aa4b8
Stefan Sedlarevic <raldesedlar@gmail.com>
2025-10-04 10:20:27 +0200

    asd

---
+++ backend/eye-training.API/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using eye_training.API.Models;
+
+namespace eye_training.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserVision> UserVisions { get; set; }
+}
